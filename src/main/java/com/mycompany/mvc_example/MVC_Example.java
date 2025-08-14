cae34c534e6b2af16bc606e97a047bc7abc1371a
ppbreno <102385983+ppbreno@users.noreply.github.com>
2025-08-14 17:14:27 -0300

    feat: first commit

---
+++ src/main/java/com/mycompany/mvc_example/MVC_Example.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mvc_example;
+
+/**
+ *
+ * @author T-Gamer
+ */
+public class MVC_Example {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
